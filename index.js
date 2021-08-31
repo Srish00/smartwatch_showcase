259887b2408f5a41c9dc8ca6cb6ef61f3780090b
Srish00 <86531505+Srish00@users.noreply.github.com>
2021-09-01 01:03:37 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,40 @@
+//red
+//https://i.imgur.com/xSIK4M8.png
+// purple
+// https://i.imgur.com/xSIK4M8.png
+
+var today = new Date();
+var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+// document.getElementById("currentTime").innerHTML = time;
+
+var timebtn = document.getElementById("time-btn");
+timebtn.addEventListener("click", function(){
+document.getElementById("currentTime").innerHTML = time;
+document.getElementById("heart-img").setAttribute("src","");
+document.getElementById("heart-rate").innerHTML = "";
+});
+
+var heartbtn = document.getElementById("heart-btn");
+heartbtn.addEventListener("click", function(){
+  document.getElementById("heart-img").setAttribute("src","https://img-premium.flaticon.com/png/512/1818/premium/1818145.png?token=exp=1630435641~hmac=21f65b1c4803285918b24dc797292d27");
+  document.getElementById("heart-rate").innerHTML = 78;
+  document.getElementById("currentTime").innerHTML = "";
+})
+
+var watchImg = document.querySelector("#watch-img")
+var blackbtn = document.querySelector(".black");
+blackbtn.addEventListener("click", function(){
+  watchImg.setAttribute("src","https://i.imgur.com/iOeUBV7.png");
+});
+var orangebtn = document.querySelector(".orange");
+orangebtn.addEventListener("click", function(){
+  watchImg.setAttribute("src","https://i.imgur.com/PTgQlim.png");
+});
+var bluebtn = document.querySelector(".blue");
+bluebtn.addEventListener("click", function(){
+  watchImg.setAttribute("src","https://i.imgur.com/Mplj1YR.png");
+});
+var purplebtn = document.querySelector(".purple");
+purplebtn.addEventListener("click", function(){
+  watchImg.setAttribute("src","https://i.imgur.com/xSIK4M8.png");
+});
