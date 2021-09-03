53175bbd5f5a1dc496f58150ba053a52b5832983
Srish00 <86531505+Srish00@users.noreply.github.com>
2021-09-03 11:00:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ heartbtn.addEventListener("click", function(){
   document.getElementById("heart-img").setAttribute("src","https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com");
   document.getElementById("heart-rate").innerHTML = 78;
   document.getElementById("currentTime").innerHTML = "";
-})
+});
 
 var watchImg = document.querySelector("#watch-img")
 var blackbtn = document.querySelector(".black");
